92301df35321bda4a507138d36a6304d0daf35dd
Christopher Luc <christopher.e.luc@gmail.com>
2019-07-26 22:49:04 -0700

    Shuffle Test
    Added in test for shuffle method

---
+++ projects/lucc/dominion/cardtest2.c
@@ -0,0 +1,47 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include "rngs.h"
+
+// set NOISY_TEST to 0 to remove printfs from output
+#define NOISY_TEST 1
+
+void assert(int value) {
+    if (NOISY_TEST) {
+        char *result =  value == 1 ? "passed" : "failed";
+        printf("Test %s\n", result);
+    }
+}
+
+int main() {
+    int i;
+    int seed = 1000;
+    int numPlayer = 2;
+    int maxBonus = 10;
+    int p, r, handCount;
+    int bonus;
+    int k[10] = {adventurer, council_room, feast, gardens, mine
+               , remodel, smithy, village, baron, great_hall};
+    struct gameState G;
+    int maxHandCount = 5;
+
+
+    if (NOISY_TEST) {
+        printf("\n\nTesting shuffle!\n");
+    }
+
+
+    r = initializeGame(numPlayer, k, seed, &G);
+    G.deck[0][0] = estate;
+    G.deck[0][1] = baron;
+    G.deck[0][2] = treasure_map;
+    G.deck[0][3] = copper;
+    G.deck[0][4] = gold;
+    G.deckCount[0] = 5;
+
+    shuffle(0, &G);
+
+    assert(G.deck[0][0] != estate || G.deck[0][1] != baron || G.deck[0][2] != treasure_map || G.deck[0][3] != copper || G.deck[0][4] != gold);
+
+}
